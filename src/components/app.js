cea418f3b733c6f7a751272fe743ac2ea7daa3a4
Hack Reactor Student <student@hackreactor.com>
2018-03-23 17:44:48 -0700

    rendered videos

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,12 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  bindings: {
+  },
+  controller: function(){
+    this.videos = exampleVideoData;
+    this.currentVideo = exampleVideoData[0];
+    console.log(this);
+  },
+  templateUrl: 'src/templates/app.html'
 });
